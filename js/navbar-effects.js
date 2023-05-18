05e91b8683c41125dad51d6e7bde1e264b0c6806
Rpfagundes <68488239+Rpfagundes@users.noreply.github.com>
2023-05-17 21:16:52 -0300

    Add files via upload

---
+++ js/navbar-effects.js
@@ -0,0 +1,32 @@
+if (window.innerWidth <= 991.98) {
+  $('#navbarSupportedContent a').click(function (e) {
+    $('#navbarSupportedContent').collapse('toggle');
+    $('#menyAvPaa').prop('checked', false);
+    $("body").toggleClass("overflow-hidden");
+    $("html").toggleClass("overflow-hidden");
+  });
+}
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 1) {
+    $('.navbar').addClass("scrolled");
+  } else {
+    $('.navbar').removeClass("scrolled");
+  }
+});
+
+$('.navbar-toggler').one('click', function() {
+  $('.navbar').addClass('scrolled');
+});
+
+$('.collapsed').click(function () {
+  $('.navbar').addClass('scrolled');
+  $(".navbar-toggler").click(function () {
+    $("body").toggleClass("overflow-hidden");
+    $("html").toggleClass("overflow-hidden");
+  });
+});
+
+$('.navbar-toggler:not(.collapsed)').click(function () {
+  $('.navbar').removeClass('scrolled');
+});
---
+++ js/slider-config.js
@@ -0,0 +1,8 @@
+$('.reviews-slider').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next")
+});
